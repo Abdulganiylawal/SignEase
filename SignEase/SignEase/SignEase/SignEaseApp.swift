d9f85f893912ad87727568fa61ddc595952a3b84
Lawal Abdulganiy <labdulghaneey@gmail.com>
2023-04-11 17:07:39 +0300

    Added the camera View

---
+++ SignEase/SignEase/SignEase/SignEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignEaseApp.swift
+//  SignEase
+//
+//  Created by Lawal Abdulganiy on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SignEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+           ContentView()
+        }
+    }
+}
